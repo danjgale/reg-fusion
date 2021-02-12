25dd13ea653299ff93f4e5412f90b88afebf4ced
Dan Gale <danjgale@gmail.com>
2021-02-12 10:47:46 -0500

    fixed up small bugs and added cli message

--- regfusion/cli.py
+++ regfusion/cli.py
@@ -1,6 +1,27 @@
 """ """
 import argparse
+import re
+import io
+import os
 from regfusion.main import vol_to_fsaverage
+
+def _cli_msg():
+
+    # read in version from __init__
+    path = os.path.join(os.path.dirname(__file__))
+    version = re.search(
+        r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+        io.open(f'{path}/__init__.py', encoding='utf_8_sig').read()
+        ).group(1)
+
+    borders = '-' * 67 + '\n'
+    msg = (f'Using regfusion {version}, a Python implementation of:\n{borders}' 
+            'Wu J, Ngo GH, Greve DN, Li J, He T, Fischl B, Eickhoff SB, Yeo BTT.\n' 
+            'Accurate nonlinear mapping between MNI volumetric and FreeSurfer \n'
+            'surface coordinate systems. Human Brain Mapping 39:3793–3808, 2018.\n'
+            f'{borders}')
+    return msg
+
 
 def _cli_parser():
     """Reads command line arguments and returns input specifications"""
@@ -11,7 +32,7 @@ def _cli_parser():
                              "in nifti format")
     parser.add_argument('-o', type=str, metavar='output_dir',
                         help="Absolute path to output directory")
-    parser.add_argument('-p', type=str, metavar='template_type', 
+    parser.add_argument('-p', type=str, metavar='template_type', default='MNI152_orig',
                         help="Type of volumetric template used in index files. "
                              "Use MNI152_orig or Colin27_orig when -r is "
                              "RF_ANTs. Use MNI152_norm or Colin27_norm when "
@@ -19,7 +40,7 @@ def _cli_parser():
                              "Ensure that the template matches the standard "
                              "space of -i (i.e., use MNI152_* if -i is "
                              "in MNI152-space). Default: MNI152_orig")
-    parser.add_argument('-r',  type=str, metavar='RF_type',
+    parser.add_argument('-r',  type=str, metavar='RF_type', default='RF_ANTs',
                         help="Type of Registration Fusion approaches used to "
                              "generate the mappings (RF_M3Z or RF_ANTs). " 
                              "RF_M3Z is recommended if data was registered " 
@@ -27,13 +48,13 @@ def _cli_parser():
                              "space using FreeSurfer. RF_ANTs is recommended " 
                              "if such registrations were carried out using " 
                              "other tools, especially ANTs. Default: RF_ANTs")
-    parser.add_argument('-i', type=str, metavar='interp', 
+    parser.add_argument('-i', type=str, metavar='interp', default='linear',
                         help="Interpolation (linear or nearest). If "
                              "-g is label.gii, then interpolation is always set "
                              "to nearest and a warning is raised. Default: "
                              "linear")
     # New flags
-    parser.add_argument('-t', type=str, metavar='out_type',
+    parser.add_argument('-t', type=str, metavar='out_type', default='nii.gz',
                         help="File type of surface files. nii.gz is true to "
                              "the original Wu et al (2018) implementation. "
                              "Note that gifti formats, either "
@@ -44,12 +65,12 @@ def _cli_parser():
 
 def main():
     params = vars(_cli_parser())
-    
+    print(_cli_msg())
     vol_to_fsaverage(
         input_img=params['s'], 
         out_dir=params['o'], 
         template_type=params['p'],
         rf_type=params['r'], 
         interp=params['i'], 
-        out_type=['t']
+        out_type=params['t']
     )
