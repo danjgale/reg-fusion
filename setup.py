3ebb1f9128bc56a5d8f19c619e4ff15c2d01b047
Dan Gale <danjgale@gmail.com>
2021-02-11 22:48:09 -0500

    set for pypi

--- setup.py
+++ setup.py
@@ -7,35 +7,46 @@ __version__ = re.search(
     io.open('regfusion/__init__.py', encoding='utf_8_sig').read()
     ).group(1)
 
-test_deps = ['pytest-cov',
-             'pytest']
-
-extras = {
-    'test': test_deps,
-}
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='regfusion',
     version=__version__,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
                                     "tests"]),
-    package_data={'regfusion': ['mappings/*.txt']},
+    package_data={
+        'regfusion': ['mappings/*']
+    },
+    include_package_data=True,
     license='MIT',
-    author='Dan Gale',
-    long_description=open('README.md').read(),
+    maintainer='Dan Gale',
+    maintainer_email="d.gale@queensu.ca",
+    description="Python implementation of registration fustion methods",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/danjgale/reg-fusion',
     install_requires=[
-        'numpy',
-        'scipy',
-        'nibabel',
-        'nilearn'
+        'numpy>=1.11',  
+        'scipy>=0.19',
+        'nibabel>=3.0.0',
+        'nilearn>=0.6.2'
     ],
-    tests_require=test_deps,
-    extras_require=extras,
+    tests_require=[
+        'pytest-cov',
+        'pytest'
+    ],
     setup_requires=['pytest-runner'],
     entry_points={
         'console_scripts': [
             'regfusion=regfusion.cli:main'
             ]
-        }
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering'
+    ]
 )
